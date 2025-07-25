a0fb6fc8dc93ce8d8393c5222931c34b2365c1af
Plainsnake915 <rifathc@icloud.com>
2025-07-25 17:38:22 -0400

    -

---
+++ src/client/java/com/hotcmds/HotcmdsClient.java
@@ -0,0 +1,10 @@
+package com.hotcmds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HotcmdsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
