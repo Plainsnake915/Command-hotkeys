b92ac39c392a0bf19584bf1737698f077a09cee1
Plainsnake915 <rifathc@icloud.com>
2026-04-19 10:53:27 -0400

    Migrated mapping and updated to 1.26

--- src/client/java/com/hotcmds/ListMenu.java
+++ src/client/java/com/hotcmds/ListMenu.java
@@ -2,28 +2,29 @@ package com.hotcmds;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.Selectable;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.EntryListWidget;
-import net.minecraft.text.Text;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.Minecraft;
+
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ObjectSelectionList;
+import net.minecraft.client.gui.screens.Screen;
+
+import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.network.chat.Component;
+import org.jspecify.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 
-import java.util.List;
-import java.util.Map;
-
 @Environment(EnvType.CLIENT)
-public class ListMenu extends Screen {
-    private Screen parent;
-    private CommandEntryListWidget entryList;
-    private ButtonWidget addButton;
-    private ButtonWidget quitButton;
+public class ListMenu extends net.minecraft.client.gui.screens.Screen {
+    private net.minecraft.client.gui.screens.Screen parent;
+    private CommandList entryList;
+    private Button addButton;
+    private Button quitButton;
 
     protected ListMenu(Screen parent) {
-        super(Text.of("Command Keybinds"));
+        super(Component.literal("Command Keybinds"));
         this.parent = parent;
 
     }
@@ -35,17 +36,17 @@ public class ListMenu extends Screen {
         int x = (this.width  - listWidth)  / 2;
         int y = (this.height - listHeight) / 2;
         int itemHeight =  25;
-        entryList = new CommandEntryListWidget(this.client, x, y-40, listWidth, listHeight, itemHeight);
-        this.addSelectableChild(entryList);
+        entryList = new CommandList(Minecraft.getInstance(), x, y-40, listWidth, listHeight, itemHeight);
+        this.addRenderableWidget(entryList);
 
-        addButton = ButtonWidget.builder(Text.of("Add New"), btn -> {
-            client.setScreen(new AddNew(this));
-        }).dimensions(this.width/2, this.height/2+100, 100, 20).build();
-        addDrawableChild(addButton);
-        quitButton = ButtonWidget.builder(Text.of("Back"), btn -> {
-            client.setScreen(parent);
-        }).dimensions(this.width/2-100,this.height/2+100,100, 20).build();
-        addDrawableChild(quitButton);
+        addButton = Button.builder(Component.literal("Add New"), btn -> {
+            Minecraft.getInstance().setScreen(new AddNew(this));
+        }).bounds(this.width/2, this.height/2+100, 100, 20).build();
+        addRenderableWidget(addButton);
+        quitButton = Button.builder(Component.literal("Back"), btn -> {
+            Minecraft.getInstance().setScreen(parent);
+        }).bounds(this.width/2-100,this.height/2+100,100, 20).build();
+        addRenderableWidget(quitButton);
 
 
 
@@ -54,25 +55,26 @@ public class ListMenu extends Screen {
 
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta){
-        super.render(context, mouseX, mouseY, delta);
+    public void extractRenderState(GuiGraphicsExtractor context, int mouseX, int mouseY, float delta){
+        super.extractRenderState(context, mouseX, mouseY, delta);
 
 
-        entryList.render(context,mouseX,mouseY,delta);
+        entryList.extractRenderState(context,mouseX,mouseY,delta);
 
 
 
 
     }
 
-    private class CommandEntryListWidget extends EntryListWidget<CommandEntryListWidget.CommandEntry> {
-        public CommandEntryListWidget(MinecraftClient minecraftClient, int x, int y, int width, int height, int itemHeight) {
-            super(minecraftClient, width, height, y, itemHeight);
+    public class CommandList extends ObjectSelectionList<CommandList.CommandEntry> {
+        public CommandList(Minecraft minecraftClient, int x, int y, int width, int height, int itemHeight) {
+            super(Minecraft.getInstance(), width, height, y, itemHeight);
             this.setX(x);
 
             for(KeyCommandPair dat: HotcmdsClient.INSTANCE.getKeybinds()){
 
                 this.addEntry(new CommandEntry(dat.command, dat.key));
+
 
 
 
@@ -88,56 +90,60 @@ public class ListMenu extends Screen {
             return this.width - 10;
         }
 
-        @Override
-        protected void appendClickableNarrations(NarrationMessageBuilder builder) {
-
-        }
 
 
-        private class CommandEntry extends EntryListWidget.Entry<CommandEntry>{
+
+
+
+        public class CommandEntry extends ObjectSelectionList.Entry<CommandList.CommandEntry> {
             private final String command;
             private final int key;
-            private ButtonWidget removeButton;
+            private Button removeButton;
+
 
 
             public CommandEntry(String command, int key) {
                 this.command = command;
                 this.key = key;
-                this.removeButton = ButtonWidget.builder(
-                                Text.literal("Remove"),
+                
+                this.removeButton = Button.builder(
+                                Component.literal("Remove"),
                                 button -> {
+
                                     HotcmdsClient.INSTANCE.removeKey(key);
                                     entryList.removeEntry(this);
+
+
                                 })
-                        .dimensions(0, 0, 60, 20) // Position will be set in render
+                        .bounds(0, 0, 60, 20) // Position will be set in render
                         .build();
+
             }
-
-
             @Override
-            public void render(DrawContext context, int mousex, int mousey, boolean hovered, float tickDelta) {
+            public void extractContent(GuiGraphicsExtractor context, int i, int j, boolean bl, float f){
                 // Get x/width/height from the parent list
                 int index = entryList.children().indexOf(this);
                 int x = entryList.getRowLeft();
                 int y = entryList.getRowTop(index);
                 int entryWidth = entryList.getRowWidth();
-                int entryHeight = entryList.itemHeight; // or entryList.getRowHeight() if available
+                int entryHeight = entryList.getRowBottom(index)-y;
+                // or entryList.getRowHeight() if available
 
                 // Background fill
-                context.fill(x, y+1, x + entryWidth, y + entryHeight-2,0xFFAAAAAA);
+                context.fill(x, y+1, x + entryWidth, y + entryHeight,0x80555555);
 
                 // Render the command-key text
-                context.drawTextWithShadow(
-                        getTextRenderer(),
-                        Text.literal("Command: " + command + ", Key: " + GLFW.glfwGetKeyName(key, 0)),
-                        x + 2, y + 2,
-                        0xFFFFFFFF
+                context.text(
+                        Minecraft.getInstance().font,
+                        Component.literal("Command: " + command + ", Key: " + GLFW.glfwGetKeyName(key, 0)),
+                        x + 2, this.getContentYMiddle()-2,
+                        0xFFFFFFFF,true
                 );
 
                 // Position and render the remove button
                 this.removeButton.setX(x + entryWidth - 70);
-                this.removeButton.setY(y + 2);
-                this.removeButton.render(context, mousex, mousey, tickDelta); // mouse coords not passed anymore
+                this.removeButton.setY(this.getContentYMiddle()-9);
+                this.removeButton.extractRenderState(context, i, j, f); // mouse coords not passed anymore
 
                 // Visibility check
                 if (removeButton.getY() < entryList.getBottom() - 20 && removeButton.getY() > entryList.getY()) {
@@ -146,11 +152,20 @@ public class ListMenu extends Screen {
                     removeButton.visible = false;
                 }
             }
+
+
             @Override
-            public boolean mouseClicked(net.minecraft.client.gui.Click click, boolean doubled) {
-                return this.removeButton.mouseClicked(click, doubled);
+            public Component getNarration() {
+                return getNarrationMessage();
+            }
+
+            @Override
+            public boolean mouseClicked(MouseButtonEvent click, boolean doubleclick) {
+                return this.removeButton.mouseClicked(click, doubleclick);
 
             }
+
+
 
 
         }
